976169597f22f0a0229fbfcdf2f965dfd2888d81
Feeshee <csjndj@gmail.com>
2018-01-11 11:38:54 -0500

    Powershell Pythagorean Theorem

---
+++ pythagoreantheorem.ps1
@@ -0,0 +1,7 @@
+﻿#Pythagorean Theorem Solver
+[double]$a = Read-Host 'What is a?'
+[double]$b = Read-Host 'What is b?'
+[double]$asq = ($a*$a)
+[double]$bsq = ($b*$b)
+[double]$c = [math]::Sqrt($asq+$bsq)
+echo $c
